0c90901f3f837f0966ce7cebf0bb7d74eb7e16e1
Alexei Dudarev <alexei.dudarev@gmail.com>
2020-02-01 21:00:53 +0200

    Webdriver init added

--- src/main/java/Utilities/CommonOperations.java
+++ src/main/java/Utilities/CommonOperations.java
@@ -1,16 +1,61 @@
 package Utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 public class CommonOperations extends Base {
     @BeforeClass
     public void startSession(){
-
+        String platform = "web";
+        if (platform.equalsIgnoreCase("web")){
+            initBrowser("Browser type");
+        }else if (platform.equalsIgnoreCase("mobile")){
+            //initMobile();
+        }else{
+            throw  new RuntimeException("invalid platform name");
+        }
     }
+
+    private static void initBrowser(String browser_type) {
+        if(browser_type.equalsIgnoreCase("chrome")){
+            driver = initChromeDriver();
+        }else if (browser_type.equalsIgnoreCase("firefox")){
+            driver = initFFDriver();
+        }else if (browser_type.equalsIgnoreCase("ie")){
+            driver = initIEDriver();
+        }else{
+            throw  new RuntimeException("invalid browser name stated");
+        }
+    }
+
+    private static WebDriver initIEDriver() {
+        WebDriverManager.iedriver().setup();
+        WebDriver driver  = new InternetExplorerDriver();
+        return driver;
+    }
+
+    private static WebDriver initFFDriver() {
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver  = new FirefoxDriver();
+        return driver;
+    }
+
+    private static WebDriver initChromeDriver() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver  = new ChromeDriver();
+        return driver;
+    }
+
     @AfterClass
     public void finishSession(){
-
+        if (driver != null){
+            driver.quit();
+        }
     }
 
 }
