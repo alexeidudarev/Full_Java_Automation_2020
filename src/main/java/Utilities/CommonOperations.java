af1feab170ccfe6f12bf1f03fa3f056043a18147
Alexei Dudarev <alexei.dudarev@gmail.com>
2020-02-01 20:30:57 +0200

    Base class added

---
+++ src/main/java/Utilities/CommonOperations.java
@@ -0,0 +1,16 @@
+package Utilities;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class CommonOperations extends Base {
+    @BeforeClass
+    public void startSession(){
+
+    }
+    @AfterClass
+    public void finishSession(){
+
+    }
+
+}
