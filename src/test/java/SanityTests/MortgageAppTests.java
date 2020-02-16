4dcacc8396ed97546c312a8f47db65ad9330919a
Alexei Dudarev <alexei.dudarev@gmail.com>
2020-02-16 14:11:22 +0200

    Nagative Test Added

--- src/test/java/SanityTests/MortgageAppTests.java
+++ src/test/java/SanityTests/MortgageAppTests.java
@@ -16,4 +16,10 @@ public class MortgageAppTests extends CommonOperations {
         MobileFlow.calculateMortgage("1000","3","4");
         Verifications.assertTextInElement(mortgageMobileAppMainActivity.txt_repayment,"£30.03");
     }
+    @Test(description = "Verify Calculating mortgage")
+    @Description("Test description : Adding values to required fields at main activity ,expected value is £30.03")
+    public void mortgageCalculatingNegative(){
+        MobileFlow.calculateMortgage("1000","3","4");
+        Verifications.assertTextInElement(mortgageMobileAppMainActivity.txt_repayment,"£50.03");
+    }
 }
